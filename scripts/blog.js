3359e2e3045728e1fb28e7c6993f5e9084c997e5
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2024-11-13 14:35:20 +0500

    added top line and preview and next

--- scripts/blog.js
+++ scripts/blog.js
@@ -11,80 +11,41 @@ function load() {
 }
 
 function render(blogs) {
+  console.log(blogs);
+  
+  blogs.sort((a, b) => {
+    return new Date(b.created_at) - new Date(a.created_at)
+  })
+  
+  console.log(blogs);
+
   listWrapper.innerHTML = "";
 
-  for (const blog of blogs) {
+  blogs.forEach((blog) => {
     listWrapper.innerHTML += `
-      <li class="list-group-item" onclick="toDetail('${blog.id}')">
-        <div class="list-item-content">
-            <div class="list-item-left">
-                <span>${blog.created_at}</span>
-                <h4>${generateTitle(blog.current_titles)}</h4>
-            </div> 
-            <div class="list-item-left"></div>
-        </div>
-    </li>
+         <li class="list-group-item" onclick="toBlogDetail('${blog.id}')">
+            <div class="list-item-content">
+                <div class="list-item-left">
+                    <span>${blog.created_at}</span>
+                    <h5 class="mt-2">${generateTitle(blog.current_titles)}</h5>
+                </div>
+                <!-- <div class="list-item-left"></div> -->
+            </div>
+        </li>
     `;
-  }
-}
-
-function toDetail(blogId) {
-  window.location.href = "../pages/blogDetail.html?id=" + blogId;
+  });
 }
 
 function generateTitle(titles) {
-  console.log(titles);
+  let cTitle = "<p>";
 
-  let wrapper = "";
-
-  for (const title of titles)
-    wrapper += `<span style="${title.style}">${title.value}</span>`;
-
-  return wrapper;
+  for (const title of titles) {
+    cTitle += `<span style="${title.style}">${title.value}</span>`;
+  }
+  cTitle += "</p>";
+  return cTitle;
 }
 
-// const listWrapper = document.querySelector("#list-wrapper");
-
-// window.onload = () => {
-//   load();
-// };
-
-// function load() {
-//   fetch("http://localhost:3000/blogs")
-//     .then((response) => response.json())
-//     .then((data) => render(data));
-// }
-
-// function render(blogs) {
-//   console.log(blogs);
-
-//   listWrapper.innerHTML = "";
-
-//   blogs.forEach((blog) => {
-//     listWrapper.innerHTML += `
-//          <li class="list-group-item" onclick="toBlogDetail('${blog.id}')">
-//             <div class="list-item-content">
-//                 <div class="list-item-left">
-//                     <span>${blog.created_at}</span>
-//                     <h5 class="mt-2">${generateTitle(blog.current_titles)}</h5>
-//                 </div>
-//                 <!-- <div class="list-item-left"></div> -->
-//             </div>
-//         </li>
-//     `;
-//   });
-// }
-
-// function generateTitle(titles) {
-//   let cTitle = "<p>";
-
-//   for (const title of titles) {
-//     cTitle += `<span style="${title.style}">${title.value}</span>`;
-//   }
-//   cTitle += "</p>";
-//   return cTitle;
-// }
-
-// function toBlogDetail(id) {
-//   window.location.href = "/pages/blogDetail.html?id=" + id;
-// }
+function toBlogDetail(id) {
+  window.location.href = "/pages/blogDetail.html?id=" + id;
+}
--- scripts/blogScript.js
+++ scripts/blogScript.js
@@ -1,19 +1,87 @@
 const blogId = new URLSearchParams(window.location.search).get("id");
 const blogDetailWrapper = document.querySelector(".blog__detail-wrapper");
 
-document.addEventListener("DOMContentLoaded", () => {
-  loadSingle();
-});
+// Line
+const topLine = document.querySelector(".top-line");
 
-function loadSingle() {
-  fetch("http://localhost:3000/blogs/" + blogId)
-    .then((response) => response.json())
-    .then((data) => renderSingle(data));
+// page links
+const previewBlogLink = document.querySelector("#preview__blog-link");
+const upcomingBlogLink = document.querySelector("#upcoming__blog-link");
+
+function findUpcomingBlog(blog, blogs) {
+  // sort asc
+  blogs.sort((a, b) => {
+    const dateA = new Date(a.created_at);
+    const dateB = new Date(b.created_at);
+    return dateB - dateA;
+  });
+
+  for (let i = 0; i < blogs.length; i++)
+    if (blogs[i].id === blog.id) return blogs[i + 1];
+
+  return undefined;
 }
 
-function renderSingle(blog) {
+function findPreviewBlog(blog, blogs) {
+  blogs.sort((a, b) => {
+    const dateA = new Date(a.created_at);
+    const dateB = new Date(b.created_at);
+    return dateB - dateA;
+  });
+
+  console.log(blogs);
+  
+  for (let i = 0; i < blogs.length; i++)
+    if (blogs[i].id == blog.id) return blogs[i - 1];
+
+  return undefined;
+}
+
+function updateTopLine() {
+  const scrollHeight = document.documentElement.scrollHeight;
+  const clientHeight = document.documentElement.clientHeight;
+
+  if (scrollHeight <= clientHeight) {
+    topLine.style.display = "none";
+    return;
+  }
+
+  const scrollPosition = window.scrollY;
+  const scrollableHeight = scrollHeight - clientHeight;
+  const scrollPercentage = (scrollPosition / scrollableHeight) * 100;
+
+  if (scrollPosition > 0) {
+    topLine.style.display = "block";
+    topLine.style.width = scrollPercentage + "%";
+  } else topLine.style.display = "none";
+}
+
+window.addEventListener("scroll", updateTopLine);
+window.addEventListener("resize", updateTopLine);
+
+window.onload = () => {
+  updateTopLine();
+
+  if (!blogId) {
+    window.location.href = "/pages/blog.html";
+    return;
+  }
+
+  load(blogId);
+};
+
+function load() {
+  fetch("http://localhost:3000/blogs")
+    .then((response) => response.json())
+    .then((data) => render(data));
+}
+
+function render(blogs) {
+
+  const blog = blogs.filter(b => b.id == blogId)[0];
+
   blogDetailWrapper.innerHTML = `
-     <div class="row">
+    <div class="row">
         <h1 class="h1">${generateTitle(blog.current_titles)}</h1>
         <div class="col-12">
             <p class="text-black-50">${blog.created_at}</p>
@@ -24,46 +92,48 @@ function renderSingle(blog) {
         ${generateBase(blog)}
     </div>
     `;
+
+    const previewBlog = findPreviewBlog(blog, blogs);
+
+    if (previewBlog) {
+      previewBlogLink.href = "http://127.0.0.1:5501/pages/blogDetail.html?id=" + previewBlog.id
+    } else {
+      previewBlogLink.innerText = ""
+    }
+
+    const upcomingBlog = findUpcomingBlog(blog, blogs);
+
+    if (upcomingBlog) {
+      upcomingBlogLink.href = "http://127.0.0.1:5501/pages/blogDetail.html?id=" + upcomingBlog.id
+    } else {
+      upcomingBlogLink.innerText = ""
+    }
 }
 
 function generateBase(blog) {
-  console.log(blog.quotes);
-  let base = blog.base;
+  let base = ``;
 
-  if (blog.images) {
-    base = generateImages(base, blog.images);
-  }
+  console.log(blog);
+
   if (blog.texts) {
-    base = generateTexts(base, blog.texts);
+    base = generateTexts(blog.base, blog.texts);
   }
   if (blog.links) {
-    base = generateLinks(base, blog.links);
+    base = generateA(base, blog.links);
   }
   if (blog.quotes) {
     base = generateQuotes(base, blog.quotes);
   }
-  if (blog.subtitles) {
-    base = generateSubtitles(base, blog.subtitles);
+  if (blog.images) {
+    base = generateImages(base, blog.images);
   }
   if (blog.lists) {
     base = generateLists(base, blog.lists);
   }
-
-  return base;
-}
-
-function generateLists(base, lists) {
-  for (const list of lists) {
-    let cList = `<${list.type}>`;
-
-    for (const item of list.items) {
-      cList += `<li class="cList">${item.value}</li>`;
-    }
-
-    cList += `</${list.type}>`;
-
-    base = base.replaceAll(list.id, cList);
+  if (blog.subtitles) {
+    base = generateSubtitles(base, blog.subtitles);
   }
+
   return base;
 }
 
@@ -77,170 +147,66 @@ function generateSubtitles(base, subtitles) {
   return base;
 }
 
-function generateQuotes(base, quotes) {
-  for (const quote of quotes)
-    base = base.replaceAll(quote.id, `<blockquote>${quote.value}</blockquote>`);
+function generateLists(base, lists) {
+  console.log(lists);
 
-  return base;
-}
+  for (const list of lists) {
+    let cList = `<${list.type}>`;
 
-function generateLinks(base, links) {
-  for (const a of links)
-    base = base.replaceAll(a.id, `<a href="${a.link}">${a.value}</a>`);
+    for (const item of list.items) {
+      cList += `<li class="cList">${item.value}</li>`;
+    }
 
-  return base;
-}
+    cList += `</${list.type}>`;
 
-function generateTexts(base, texts) {
-  for (const text of texts)
-    base = base.replaceAll(text.id, `<p>${text.value}</p>`);
+    base = base.replaceAll(list.id, cList);
+  }
 
   return base;
 }
 
 function generateImages(base, images) {
-  for (const img of images)
+  for (const image of images) {
     base = base.replaceAll(
-      img.id,
-      `<img class="w-100" src="${img.url}" title="${img.title}" alt="${img.alt}"/>`
+      image.id,
+      `<img class="w-100 mt-1 mb-2" src="${image.url}" alt="${image.alt}">`
     );
+  }
+
+  return base;
+}
+
+function generateQuotes(base, quotes) {
+  for (const quote of quotes) {
+    base = base.replaceAll(quote.id, `<blockquote>${quote.value}</blockquote>`);
+  }
+  return base;
+}
+
+function generateTexts(base, texts) {
+  for (const text of texts)
+    base = base.replaceAll(text.id, `<p class="justify">${text.value}</p>`);
+
+  return base;
+}
+
+function generateA(base, as) {
+  for (const a of as) {
+    base = base.replaceAll(
+      a.id,
+      `<a style="${a.style}" href="${a.link}">${a.value}</a>`
+    );
+  }
 
   return base;
 }
 
 function generateTitle(titles) {
-  return titles
-    .map((title) => `<span style="${title.style}">${title.value}</span>`)
-    .join("");
+  let cTitle = "<p>";
+
+  for (const title of titles) {
+    cTitle += `<span style="${title.style}">${title.value}</span>`;
+  }
+  cTitle += "</p>";
+  return cTitle;
 }
-
-// const blogId = new URLSearchParams(window.location.search).get("id");
-// const blogDetailWrapper = document.querySelector(".blog__detail-wrapper");
-
-// window.onload = () => {
-//   if (!blogId) {
-//     window.location.href = "/pages/blog.html";
-//     return;
-//   }
-
-//   load(blogId);
-// };
-
-// function load(blogId) {
-//   fetch("http://localhost:3000/blogs/" + blogId)
-//     .then((response) => response.json())
-//     .then((data) => render(data));
-// }
-
-// function render(blog) {
-//   blogDetailWrapper.innerHTML = `
-//     <div class="row">
-//         <h1 class="h1">${generateTitle(blog.current_titles)}</h1>
-//         <div class="col-12">
-//             <p class="text-black-50">${blog.created_at}</p>
-//         </div>
-//     </div>
-//     <div id="line"></div>
-//     <div class="blog__detail-content">
-//         ${generateBase(blog)}
-//     </div>
-//     `;
-// }
-
-// function generateBase(blog) {
-//   let base = ``;
-
-//   console.log(blog);
-
-//   if (blog.texts) {
-//     base = generateTexts(blog.base, blog.texts);
-//   }
-//   if (blog.links) {
-//     base = generateA(base, blog.links);
-//   }
-//   if (blog.quotes) {
-//     base = generateQuotes(base, blog.quotes);
-//   }
-//   if (blog.images) {
-//     base = generateImages(base, blog.images);
-//   }
-//   if (blog.lists) {
-//     base = generateLists(base, blog.lists);
-//   }
-//   if (blog.subtitles) {
-//     base = generateSubtitles(base, blog.subtitles);
-//   }
-
-//   return base;
-// }
-
-// function generateSubtitles(base, subtitles) {
-//   for (const subtitle of subtitles) {
-//     base = base.replaceAll(subtitle.id, `<h4 class="mt-4 mb-4">${subtitle.value}</h4>`);
-//   }
-//   return base;
-// }
-
-// function generateLists(base, lists) {
-//   console.log(lists);
-
-//   for (const list of lists) {
-//     let cList = `<${list.type}>`;
-
-//     for (const item of list.items) {
-//       cList += `<li class="cList">${item.value}</li>`;
-//     }
-
-//     cList += `</${list.type}>`;
-
-//     base = base.replaceAll(list.id, cList);
-//   }
-
-//   return base;
-// }
-
-// function generateImages(base, images) {
-//   for (const image of images) {
-//     base = base.replaceAll(
-//       image.id,
-//       `<img class="w-100 mt-1 mb-2" src="${image.url}" alt="${image.alt}">`
-//     );
-//   }
-
-//   return base;
-// }
-
-// function generateQuotes(base, quotes) {
-//   for (const quote of quotes) {
-//     base = base.replaceAll(quote.id, `<blockquote>${quote.value}</blockquote>`);
-//   }
-//   return base;
-// }
-
-// function generateTexts(base, texts) {
-//   for (const text of texts)
-//     base = base.replaceAll(text.id, `<p class="justify">${text.value}</p>`);
-
-//   return base;
-// }
-
-// function generateA(base, as) {
-//   for (const a of as) {
-//     base = base.replaceAll(
-//       a.id,
-//       `<a style="${a.style}" href="${a.link}">${a.value}</a>`
-//     );
-//   }
-
-//   return base;
-// }
-
-// function generateTitle(titles) {
-//   let cTitle = "<p>";
-
-//   for (const title of titles) {
-//     cTitle += `<span style="${title.style}">${title.value}</span>`;
-//   }
-//   cTitle += "</p>";
-//   return cTitle;
-// }
